59767f67c19c29f90cfd7a606112ac35d65d65db
mustafaguler3 <mustafa.glr@hotmail.com>
2022-02-19 17:27:43 +0300

    quick sort

--- Diagrams/MovieManagement/README.md
+++ Diagrams/MovieManagement/README.md
@@ -1,2 +1,1 @@
 # kodluyoruz-JavaBackend-projectsss
-afsafsafsafsf
---
+++ QuickSort/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int arr[] = {10, 7, 8, 9, 1, 5};
+        int n = arr.length;
+
+        QuickSort ob = new QuickSort();
+        ob.sort(arr, 0, n-1);
+
+        System.out.println("sorted array");
+        ob.printArray(arr);
+    }
+
+
+
+}
